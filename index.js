a7464a4098acb9ec0b071aa5932fb7256abbf7d9
destbest <be.destbest@gmail.com>
2020-03-20 18:16:11 +0100

    Working signup bot
    Co-authored-by: EstefanTT <estefantt@gmail.com>

---
+++ index.js
@@ -0,0 +1,145 @@
+const fs = require('fs')
+const Telebot = require('telebot')
+const ecc = require('eosjs-ecc')
+
+// @ts-ignore
+const config = require('./config.json')
+
+const bot = new Telebot({
+    token: config.keys.bot,
+    usePlugins: ['shortReply']
+})
+
+const { Api, JsonRpc, RpcError } = require('eosjs');
+const { JsSignatureProvider } = require('eosjs/dist/eosjs-jssig');      
+const fetch = require('node-fetch');                                    
+const { TextEncoder, TextDecoder } = require('util');                   
+const privateKeys = [config.keys.wax];
+const signatureProvider = new JsSignatureProvider(privateKeys);
+// @ts-ignore
+const rpc = new JsonRpc(config.apiEndpoint, { fetch });
+const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
+
+// ##############################################################################################################
+// #################################################  WAX  ######################################################
+// ##############################################################################################################
+
+async function transfer (memo) {
+    try {
+        await api.transact(
+            {
+                actions: [{
+                    account: 'eosio.token',
+                    name: 'transfer',
+                    authorization: [{
+                        actor: config.waxAccountName,
+                        permission: 'active',
+                    }],
+                    data: {
+                        from: config.waxAccountName,
+                        to: config.waxSmartContractName,
+                        quantity: config.waxAmount,
+                        memo: memo,
+                    },
+                }]
+            }, {
+                blocksBehind: 3,
+                expireSeconds: 30
+            }
+        )
+        return true
+    } catch (e) {
+        return e
+    }
+}
+
+async function checkIfNameIsAvailable (accountName) {
+    try { 
+        await rpc.get_account(accountName) 
+        return false
+    }
+    catch (e) {
+        if (e.json && e.json.code === 500) return true
+        else return e
+    }
+}
+
+// ##############################################################################################################
+// #################################################  BOT  ######################################################
+// ##############################################################################################################
+
+const validAccountNameCharacters = ['.', '1', '2', '3', '4', '5', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 
+            'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            
+bot.on('/new_account', async (msg) => {
+    // Don't accept requests from Telegram bots
+    if (msg.from.is_bot) {
+        bot.sendMessage(msg.chat.id, `😔 Sorry, bots are not allowed to create accounts`)
+        return
+    }
+
+    // Extracting accountName and publicKey from user msg
+    let [accountName, publicKey] = msg.text.split(' ').slice(1, 3)
+    accountName = accountName ? accountName.toLowerCase() : undefined
+
+    // Checking for name and key validity
+    const invalidCharaters = []
+    let hasDotAtInvalidPos = false
+    let isAccountNameAvailable = await checkIfNameIsAvailable(accountName)
+    let isPubKeyValid = ecc.isValidPublic(publicKey)
+    let error = false
+    
+    if (accountName && accountName.length === 12) {
+        accountName.split('').forEach((char, i) => {
+            if (!validAccountNameCharacters.includes(char)) invalidCharaters.push(char)
+            if ((i === 0 || i === 11) && char === '.') hasDotAtInvalidPos = true
+        })
+        if (invalidCharaters.length === 0 && !hasDotAtInvalidPos) {
+            const res = await checkIfNameIsAvailable(accountName)
+            if (typeof res !== 'object') isAccountNameAvailable = res
+            else {
+                console.log(res)
+                error = res
+            }
+        }
+    }
+    
+    // Error message
+    if (error) {bot.sendMessage(msg.chat.id, `😔 Sorry, an error occured.`)}
+    else if (!accountName || !publicKey) bot.sendMessage(msg.chat.id, `😔 Sorry, you need to provide accountName & publicKey`)
+    else if (accountName && accountName.length !== 12) bot.sendMessage(msg.chat.id, `😔 Sorry, your account name must be 12 characters long, no more, no less.`)
+    else if (invalidCharaters.length !== 0) bot.sendMessage(msg.chat.id, `😔 Sorry, the following character(s) are not allowed: \n${invalidCharaters.join('  ')}`)
+    else if (hasDotAtInvalidPos) bot.sendMessage(msg.chat.id, `😔 Sorry, you cannot use dots ( . ) for the first nor the last character of your account name.`)
+    else if (!isAccountNameAvailable) bot.sendMessage(msg.chat.id, `😔 Sorry, this account name is already taken.`)
+    else if (!isPubKeyValid) bot.sendMessage(msg.chat.id, `😔 Sorry, this public key is not valid.`)
+    
+    // Create account process
+    else {
+        // @ts-ignore
+        let blackListedUserIds = require('./blackListedUserIds.json')
+        if (!blackListedUserIds.includes(msg.from.id)) {
+            bot.sendMessage(msg.chat.id, "Account creation in progress... ⏳")
+            let isCreated = await transfer(accountName + '-' + publicKey)
+            if (isCreated) {
+                blackListedUserIds.push(msg.from.id)
+                fs.writeFileSync('./blackListedUserIds.json', JSON.stringify(blackListedUserIds))
+                bot.sendMessage(msg.chat.id, `✅ Account created \nhttps://wax.bloks.io/account/${accountName}`)
+            }
+        } else {
+            bot.sendMessage(msg.chat.id, `😔 Sorry, you already have created a account.`)
+        }
+    }
+})
+
+bot.on(['/help', '/start'], (msg) => {
+    bot.sendMessage(msg.chat.id, 
+`Use /new_account accountName publicKey 
+        
+Account names should be 12 characters long, no more, no less.
+Account names should only contain letters [A-Z], numbers [1-5] 
+Account names can contain optionnal dots . except for the first and last characters.`
+)})
+
+bot.on('text', (msg) => {console.log(msg)})
+
+bot.connect()
---
+++ readMe.md
@@ -0,0 +1,12 @@
+# telegram-wax-signup
+
+How to install:
+---------------
+
+1) Create your bot via Telegram @BotFather
+2) Add bot to Wax Telegram group
+3) Deactivate "Allow Groups?" functionality in bot setting via Telegram @BotFather
+4) Put bot token (key) in config.json > keys.bot
+5) Fill the rest of the config.json file
+6) Install the dependencies: npm i
+6) Launch the script: node index.js
